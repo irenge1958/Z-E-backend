c54992ff7ea481db835143f2f053f21e4a1d0c1d
Irenge Bahizire <eshibahizire@gmail.com>
2025-10-16 11:25:42 +0800

    first

--- index.js
+++ index.js
@@ -75,6 +75,3 @@ return res.status(status).json({
 app.get('/', (req, res) => {
   res.status(200).send('API is working on Vercell!');
 });
-app.listen(6000,()=>{
-     console.log("The server runs on 6000")
-})
