5d51bf472c45152455436d78f0e50be60a2b0c49
Irenge Bahizire <eshibahizire@gmail.com>
2025-10-16 11:37:00 +0800

    try

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express=require("express")
-const morgan=require("morgan")
-const helmet=require("helmet")
 const dotenv=require("dotenv")
 const authRoute=require("./route/auth")
 const user=require("./route/user")
